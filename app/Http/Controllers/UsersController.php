583f929fa9b1cfd8c00fc4de3f0ad746b44374cc
Henry Lee <henryleeworld@gmail.com>
2023-04-26 00:20:59 +0800

    feat:初始化

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use DB;
+use Spatie\SqlCommenter\SqlCommenter;
+
+class UsersController extends Controller 
+{
+    public function show() 
+    {
+        SqlCommenter::enable();
+        SqlCommenter::addComment('verified_by', 'henry');
+        DB::enableQueryLog();
+        User::where('name', 'Taiwan number one')->first();
+        SqlCommenter::disable();
+        dd(DB::getQueryLog());
+    }
+}
